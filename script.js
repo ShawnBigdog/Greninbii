c86edc0d2b59ad7a0b5c3e405bf7d1859750b1ed
ShawnBigdog <140444687+ShawnBigdog@users.noreply.github.com>
2023-07-28 14:09:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 var form = 0;
 
-function evolve(){
-  if(form == 0){
+function evolve() {
+  if (form == 0) {
     form++;
-   document.getElementById("froak").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/657.png";
-  } else if(form == 1){
+    document.getElementById("froak").src =
+      "https://assets.pokemon.com/assets/cms2/img/pokedex/full/657.png";
+  } else if (form == 1) {
     form++;
-    document.getElementById("froak").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/658.png";
+    document.getElementById("froak").src =
+      "https://assets.pokemon.com/assets/cms2/img/pokedex/full/658.png";
   } else {
     form = 0;
-    document.getElementById("froak").src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/656.png";
+    document.getElementById("froak").src =
+      "https://assets.pokemon.com/assets/cms2/img/pokedex/full/656.png";
   }
+}
+
+function cry() {
+  var audio = new Audio(
+    "https://play.pokemonshowdown.com/audio/cries/greninja.ogg"
+  );
+  audio.play();
 }
